394a99d3f3f6605ff1622c4ddeb06bcbb71a6ad0
likas369 <l.kasmenis@gmail.com>
2024-12-03 14:27:09 +0000

    api

---
+++ index.js
@@ -0,0 +1,38 @@
+// Import
+ 
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 8080;
+
+// Listener für express-instanz
+app.listen(
+    PORT,
+    () => console.log(`server ready at http://localhost"${PORT}`)
+);
+
+// /* 200 OK */ 
+// app.get('/', (req, res) => {
+//     res.sendStatus(200);
+//   });
+
+/* JSON */ 
+// app.get("/", (req,res) =>
+// {
+//     res.send({
+//         item: "T-shirt",
+//         size: "L",
+//         onstock: 20
+//     })
+// });
+
+/* HTML */ 
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'src', 'boo.html'));
+//     });
+
+/* JPG */ 
+// app.get('/', (req, res) => {
+//       res.sendFile(path.join(__dirname, 'src', '404.jpg'));
+//     });
+    
